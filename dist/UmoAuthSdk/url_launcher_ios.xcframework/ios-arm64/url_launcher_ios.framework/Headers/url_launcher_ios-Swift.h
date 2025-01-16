bc70ea1e848cde7edf57e7ba8ccf74a626aed188
anand-kulk <70729249+anand-kulk@users.noreply.github.com>
2025-01-16 15:42:15 -0800

    releasing v0.0.9

---
+++ dist/UmoAuthSdk/url_launcher_ios.xcframework/ios-arm64/url_launcher_ios.framework/Headers/url_launcher_ios-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3eb96256fba25df9f25d20e9013dba0d5e3913ac73b48bad01b3aae1c853545
+size 10663
---
+++ dist/UmoAuthSdk/url_launcher_ios.xcframework/ios-arm64_x86_64-simulator/url_launcher_ios.framework/Headers/url_launcher_ios-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:17e5f8717c5e57005b82fd9c55e74c79553e4931da543ce217448534f49606fe
+size 21271
