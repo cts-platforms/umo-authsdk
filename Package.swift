657288162cbcc2e4b9361544ce04283fd4169957
patrickdunsheecubic <69591893+patrickdunsheecubic@users.noreply.github.com>
2025-08-19 11:06:39 -0500

    NEX-6759 Support Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,76 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "UmoAuthSdk",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "UmoAuthSdk",
+            targets: [
+                "UmoAuthSdk",
+                "App",
+                "Flutter",
+                "FlutterPluginRegistrant",
+                "amplify_auth_cognito",
+                "amplify_secure_storage",
+                "device_info_plus",
+                "package_info_plus",
+                "path_provider_foundation",
+                "sms_autofill",
+                "url_launcher_ios"
+            ]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .binaryTarget(
+            name: "App",
+            path: "dist/UmoAuthSdk/App.xcframework"
+        ),
+        .binaryTarget(
+            name: "Flutter",
+            path: "dist/UmoAuthSdk/Flutter.xcframework"
+        ),
+        .binaryTarget(
+            name: "FlutterPluginRegistrant",
+            path: "dist/UmoAuthSdk/FlutterPluginRegistrant.xcframework"
+        ),
+        .binaryTarget(
+            name: "amplify_auth_cognito",
+            path: "dist/UmoAuthSdk/amplify_auth_cognito.xcframework"
+        ),
+        .binaryTarget(
+            name: "amplify_secure_storage",
+            path: "dist/UmoAuthSdk/amplify_secure_storage.xcframework"
+        ),
+        .binaryTarget(
+            name: "device_info_plus",
+            path: "dist/UmoAuthSdk/device_info_plus.xcframework"
+        ),
+        .binaryTarget(
+            name: "package_info_plus",
+            path: "dist/UmoAuthSdk/package_info_plus.xcframework"
+        ),
+        .binaryTarget(
+            name: "path_provider_foundation",
+            path: "dist/UmoAuthSdk/path_provider_foundation.xcframework"
+        ),
+        .binaryTarget(
+            name: "sms_autofill",
+            path: "dist/UmoAuthSdk/sms_autofill.xcframework"
+        ),
+        .binaryTarget(
+            name: "url_launcher_ios",
+            path: "dist/UmoAuthSdk/url_launcher_ios.xcframework"
+        ),
+        .binaryTarget(
+            name: "UmoAuthSdk",
+            path: "dist/UmoAuthSdk/UmoAuthSdk.xcframework"
+        )
+    ]
+)
